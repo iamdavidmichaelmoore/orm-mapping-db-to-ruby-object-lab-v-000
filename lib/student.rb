e978956cf8e4583983c17c3d02001304a2f93306
David Moore <hey@iamdavidmoore.com>
2018-07-04 23:02:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
-    SELECT * FROM songs WHERE name = ? LIMIT 1
+      SELECT * FROM songs WHERE name = ? LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
